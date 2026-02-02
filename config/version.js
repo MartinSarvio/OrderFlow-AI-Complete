8967595807c29a5dd17a186e0917c7967f8cb4a1
MartinSarvio <MartinSarvio@hotmail.com>
2026-02-02 16:41:14 +0100

    Tweak app preview text size and positioning

--- config/version.js
+++ config/version.js
@@ -1,14 +1,14 @@
 // OrderFlow Version Configuration
 // Single source of truth for version information
 const VERSION_CONFIG = {
-  version: '3.3.3',
-  build: 333,
-  cacheName: 'orderflow-v333',
+  version: '3.3.4',
+  build: 334,
+  cacheName: 'orderflow-v334',
   releaseDate: '2026-02-02',
 
   // Sidebar Template Metadata
   sidebarTemplate: {
-    version: '3.3.3',
+    version: '3.3.4',
     protected: true,
     authorizationRequired: true,
     lastModified: '2026-02-02'
